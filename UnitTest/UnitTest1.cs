adcc43279122f369795d4188828fd9316cd8cdf4
Phi Long Nguyen <kapustacab@gmail.com>
2017-03-16 09:58:51 +0100

    Unit tests added

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
