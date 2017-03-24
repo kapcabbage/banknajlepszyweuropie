8d91815892ab4c82f0a2fed59dd82c5b6e45f9a6
Mustrumion <bar.popiel@gmail.com>
2017-03-24 10:19:11 +0100

    Poprawki lokaty, testy lokaty

---
+++ NajlepszyBankSAUnitTest/LokataTests.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NajlepszyBankSA;
+using NajlepszyBankSA.Interfejsy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NajlepszyBankSA.Tests
+{
+    [TestClass()]
+    public class LokataTests
+    {
+        private Lokata _lokata;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _lokata = new Lokata(Guid.NewGuid().ToString(), new WłaścicielMock(), new BankMock(), 1000M);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void SaldoNieMożnaModyfikować()
+        {
+            _lokata.Saldo -= 100M;
+        }
+
+        private class WłaścicielMock : IWłaściciel
+        {
+            public string KodPocztowy
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public string Miasto
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public string Nazwa
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public string NumerDomu
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public string NumerLokalu
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public string PełenAdres
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+            }
+
+            public string Ulica
+            {
+                get
+                {
+                    throw new NotImplementedException();
+                }
+
+                set
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        private class BankMock : IBank
+        {
+            public void Wykonaj(IOperacjaBankowa operacja)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
